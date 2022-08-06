9da97d0d1ec803990cf632fc6f743a6889fdbf70
Rafael Araujo <raraujo@drcontactlens.com>
2022-08-06 12:06:13 -0300

    #C01A07 Add improvements

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import style from './Botao.module.scss';
 
-class Button extends React.Component<{type?: 'button' | 'submit' | 'reset' | undefined, onClick?: () => void}> {
-  render() {
-    const {type = 'button', onClick} = this.props;
-    return <button onClick={onClick} className={style.botao} type={type}>{this.props.children}</button>;
-  }
+interface Props {
+  onClick?: () => void,
+  type?: "button" | "submit" | "reset" | undefined
+  children?: React.ReactNode,
+}
+
+function Button({onClick, type, children}: Props) {
+  return(
+    <button onClick={onClick} className={style.botao} type={type}>{children}</button>
+  )
 }
 
 export default Button;
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { v4 as uuidv4 } from 'uuid';
 
@@ -6,57 +6,59 @@ import Button from '../Button';
 import { ITask } from '../types/tasks';
 import style from './Form.module.scss';
 
-class Form extends React.Component<{setTask: React.Dispatch<React.SetStateAction<ITask[]>>}> {
-  state = {
-    task: '',
-    time: '00:00'
-  }
+interface Props {
+  setTask: React.Dispatch<React.SetStateAction<ITask[]>>
+}
 
-  addTask(event: React.FormEvent<HTMLFormElement>) {
+function Form({setTask}: Props) {
+
+  const [newTask, setNewTask ] = useState<string>('');
+  const [newTime, setNewTime] = useState<string>('00:00');
+
+  const addTask = (event: React.FormEvent<HTMLFormElement>)  => {
     event.preventDefault();
-    this.props.setTask(oldTasks => [...oldTasks, {...this.state, isSelected: false, isCompleted: false, uuid: uuidv4()}] );
-    this.setState({
-      task: '',
-      time: '00:00'
-    })
+    
+    setTask(oldTasks => [...oldTasks, {task: newTask, time: newTime, isSelected: false, isCompleted: false, uuid: uuidv4()}] );
+    
+    setNewTask('');
+    setNewTime("00:00");
   }
 
-  render() {
-    return (
-      <form className={style.novaTarefa} onSubmit={this.addTask.bind(this)}>
-        <div className={style.inputContainer}>
-          <label htmlFor="task">Add new subject</label>
-          <input
-            type="text"
-            name="task"
-            id="task"
-            placeholder="Task"
-            required
-            onChange={(event) => this.setState({ task: event.target.value })}
-            value={this.state.task}
-          />
-        </div>
-        <div className={style.inputContainer}>
-          <label htmlFor="time">Time</label>
-          <input
-            type="time"
-            step={1}
-            min="00:00:00"
-            max="01:30:00"
-            name="time"
-            id="time"
-            placeholder="Time"
-            required
-            onChange={(event) => this.setState({ time: event.target.value })}
-            value={this.state.time}
-          ></input>
-        </div>
-        <Button type='submit'>
-          Adicionar
-        </Button>
-      </form>
-    );
-  }
+  return (
+    <form className={style.novaTarefa} onSubmit={addTask}>
+    <div className={style.inputContainer}>
+      <label htmlFor="task">Add new subject</label>
+      <input
+        type="text"
+        name="task"
+        id="task"
+        placeholder="Task"
+        required
+        onChange={(event) => setNewTask(event.target.value)}
+        value={newTask}
+      />
+    </div>
+    <div className={style.inputContainer}>
+      <label htmlFor="time">Time</label>
+      <input
+        type="time"
+        step={1}
+        min="00:00:00"
+        max="01:30:00"
+        name="time"
+        id="time"
+        placeholder="Time"
+        required
+        onChange={(event) => setNewTime(event.target.value)}
+        value={newTime}
+      ></input>
+    </div>
+    <Button type='submit'>
+      Adicionar
+    </Button>
+  </form>
+  )
+
 }
 
 export default Form;
